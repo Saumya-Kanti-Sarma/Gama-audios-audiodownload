fc52b9548532bc82737503d43d7ae6b9bba0fb7f
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-06-30 13:24:43 +0530

    added: logging

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify
 from pytube import YouTube
 import os
-
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def hello_world():
     return jsonify({
         "msg" : "hello coders.."
     })
-
 
 @app.route('/api/<id>')
 def downloadMusic(id):
@@ -19,11 +19,21 @@ def downloadMusic(id):
     yt = YouTube(f"{URL}")
     audio_streams = yt.streams.filter(only_audio=True).get_by_itag(251)
     audio_name = f"{yt.title}_From_Gama_Audios_"
+
+    # declearing download path
     download_path = os.path.join(os.path.expanduser("~"), "Downloads")
     folder = "GamaAudios"
     folder_path = os.path.join(download_path,folder)
+
+    # declearing logging infos
+    logging.info(f"Download path: {download_path}")
+    logging.info(f"Folder path: {folder_path}")
+    
     if not os.path.exists(folder_path):
       os.makedirs(folder_path)
+      logging.info(f"Created folder: {folder_path}")
+
+      
     audio_streams.download(output_path=folder_path)
 
     return jsonify({
@@ -36,9 +46,6 @@ def downloadMusic(id):
     return jsonify({
         "We have a error" : e
     })
-
-
-
 
 
 if __name__ == '__main__':
