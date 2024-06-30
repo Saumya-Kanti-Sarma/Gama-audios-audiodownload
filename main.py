1e5120c9e56752ea107f6e535fabd8ebd0c9fbe5
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-06-30 13:47:49 +0530

    i think it worked

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file, request
 from pytube import YouTube
 import os
-import logging
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def hello_world():
     return jsonify({
-        "msg" : "hello coders.."
+        "msg": "hello coders.."
     })
 
 @app.route('/api/<id>')
-def downloadMusic(id):
-  URL = f"https://youtu.be/{id}"
-  try:
-    yt = YouTube(f"{URL}")
-    audio_streams = yt.streams.filter(only_audio=True).get_by_itag(251)
-    audio_name = f"{yt.title}_From_Gama_Audios_"
+def get_audio_url(id):
+    URL = f"https://youtu.be/{id}"
+    try:
+        yt = YouTube(f"{URL}")
+        audio_streams = yt.streams.filter(only_audio=True).get_by_itag(251)
+        download_path = os.path.join(os.path.expanduser("~"), "Downloads")
+        folder = "GamaAudios"
+        folder_path = os.path.join(download_path, folder)
 
-    # declearing download path
-    download_path = os.path.join(os.path.expanduser("~"), "Downloads")
-    folder = "GamaAudios"
-    folder_path = os.path.join(download_path,folder)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
-    # declearing logging infos
-    logging.info(f"Download path: {download_path}")
-    logging.info(f"Folder path: {folder_path}")
-    
-    if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
-      logging.info(f"Created folder: {folder_path}")
+        audio_file_path = audio_streams.download(output_path=folder_path)
+        audio_file_name = os.path.basename(audio_file_path)
 
-      
-    audio_streams.download(output_path=folder_path)
+        return jsonify({
+            "audio_url": f"/download/{audio_file_name}"
+        })
 
-    return jsonify({
-       "Title" : audio_name,
-       "quality" : str(audio_streams),
-       "status" : 200
-    })
+    except Exception as e:
+        return jsonify({
+            "We have an error": str(e)
+        })
 
-  except Exception as e:
-    return jsonify({
-        "We have a error" : e
-    })
-
+@app.route('/download/<filename>')
+def download_file(filename):
+    download_path = os.path.join(os.path.expanduser("~"), "Downloads", "GamaAudios", filename)
+    return send_file(download_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
