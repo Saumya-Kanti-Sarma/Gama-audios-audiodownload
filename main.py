765453f44ac91fa914f8c47b1064d57191ec3e7b
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-06-30 21:48:36 +0530

    improved msgs

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ CORS(app) #using CORS to be able to fetch data easily
 @app.route('/')
 def hello_world():
     response =  jsonify({
-        "msg": "hello coders.."
+        "msg": "hello coders..",
+        "Mp3": "/api/mp3/",
+        "mp4": "Processing..",
+        "status": 200
     })
     response.headers.add("Content-Type", 'application/json')
     return response
 
-@app.route('/api/<id>')
+@app.route('/api/mp3/<id>')
 def get_audio_url(id):
     # kJQP7kiw5Fk&pp=ygUJZGVzcGFjaXRv
     URL = f"https://www.youtube.com/watch?v={id}"
@@ -32,12 +35,16 @@ def get_audio_url(id):
         audio_file_name = os.path.basename(audio_file_path)
 
         return jsonify({
+            "status-code" : 200,
+            "status" : "download successfull",
             "audio_url": f"/download/{audio_file_name}"
         })
 
     except Exception as e:
         return jsonify({
-            "We have an error": str(e)
+            "We have an error": str(e),
+            "status-code" : 505,
+            "status" : "download un-successfull",
         })
 
 @app.route('/download/<filename>')
