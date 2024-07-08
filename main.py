147e5931f4f299216a9a23e08704f37972a835ca
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-07-08 15:37:53 +0530

    improved downloads

--- main.py
+++ main.py
@@ -25,19 +25,15 @@ def get_audio_url(id):
         yt = YouTube(f"{URL}")
         audio_streams = yt.streams.filter(only_audio=True).get_by_itag(251)
         download_path = os.path.join(os.path.expanduser("~"), "Downloads")
-        folder = "GamaAudios"
-        folder_path = os.path.join(download_path, folder)
 
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        audio_file_path = audio_streams.download(output_path=folder_path)
-        audio_file_name = os.path.basename(audio_file_path)
+        audio_file_path = audio_streams.download(output_path=download_path)
+        base, ext = os.path.splitext(audio_file_path) 
+        new_file = base + '.mp3'
+        os.rename(audio_file_path, new_file) 
 
         return jsonify({
             "status-code" : 200,
             "status" : "download successfull",
-            "audio_url": f"/download/{audio_file_name}"
         })
 
     except Exception as e:
@@ -46,11 +42,6 @@ def get_audio_url(id):
             "status-code" : 505,
             "status" : "download un-successfull",
         })
-
-@app.route('/download/<filename>')
-def download_file(filename):
-    download_path = os.path.join(os.path.expanduser("~"), "Downloads", "GamaAudios", filename)
-    return send_file(download_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
